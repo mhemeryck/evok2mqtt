8638c68bf429c7d8a070756cc6248ad602ab4585
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2019-11-09 16:19:41 +0100

    Adds logging / argparse / main

--- evok2mqtt.py
+++ evok2mqtt.py
@@ -1,22 +1,63 @@
 #!/usr/bin/env python
+import argparse
 import asyncio
 import json
+import logging
+import logging.config
+import sys
 
 import websockets
 
 URI = "ws://tesla.lan/ws"
 
+# Log setup
+LOG_LEVEL = logging.INFO
+LOG_CONFIG = dict(
+    version=1,
+    formatters={
+        "default": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+    },
+    handlers={
+        "stream": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "level": LOG_LEVEL,
+        }
+    },
+    root={"handlers": ["stream"], "level": LOG_LEVEL},
+)
+
+logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger(__name__)
+
 
 async def process(payload):
     obj = json.loads(payload)[0]
-    print(obj)
+    logging.info(obj)
 
 
-async def main():
-    async with websockets.connect(URI) as websocket:
+async def connect(uri):
+    logging.info("Connecting to %s", uri)
+    async with websockets.connect(uri) as websocket:
         while True:
             payload = await websocket.recv()
             await (process(payload))
 
 
-asyncio.get_event_loop().run_until_complete(main())
+def _parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--uri", help="unipi websocket URI", default=URI)
+    return parser
+
+
+def main():
+    # Parse
+    parser = _parser()
+    args = parser.parse_args()
+
+    # Loop
+    asyncio.get_event_loop().run_until_complete(connect(args.uri))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
